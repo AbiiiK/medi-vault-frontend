57920c6f41b205196d35f46e7ca84057676c32bb
Abiiik <abinayan.cs.com>
2026-05-04 07:13:54 +0530

    Update MediVault frontend

--- test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:medi_vault/main.dart';
-
-void main() {
-  testWidgets('App smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const MediVaultApp());
-  });
-}
